f00ede3abd3c738365cee41530df52b115a3df2f
Suresh Nemani <chandra.nemani@gmail.com>
2016-06-20 16:33:32 +0530

    "change1"

--- Git_Exp2/src/Gitpack2_1/Exppro1.java
+++ Git_Exp2/src/Gitpack2_1/Exppro1.java
@@ -5,7 +5,7 @@ public class Exppro1 {
 	public static void main(String[] args) {
 		System.out.println("line 1");
 		System.out.println("line 2 after path change");
-
+        System.out.println("first change");
 	}
 
 }
