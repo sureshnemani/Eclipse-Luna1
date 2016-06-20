1ebfb1f523aa8f99fee414e05c85fcc16b610bd1
Suresh Nemani <chandra.nemani@gmail.com>
2016-06-20 16:10:14 +0530

    Initial commit

---
+++ Git _Exp1/src/Gitpack1/Add1.java
@@ -0,0 +1,13 @@
+package Gitpack1;
+
+public class Add1 {
+	public static int a=10;
+	public static int b=20;
+
+	public static void main(String[] args) {
+		System.out.println("Addition is :"+(a+b));
+		System.out.println("1st git commit");
+
+	}
+
+}
