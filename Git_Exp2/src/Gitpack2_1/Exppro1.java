c08b885287f0afdaf8552397c90c4967b2fac1cd
Suresh Nemani <chandra.nemani@gmail.com>
2016-06-20 16:32:13 +0530

    "init commit"

---
+++ Git_Exp2/src/Gitpack2_1/Exppro1.java
@@ -0,0 +1,11 @@
+package Gitpack2_1;
+
+public class Exppro1 {
+
+	public static void main(String[] args) {
+		System.out.println("line 1");
+		System.out.println("line 2 after path change");
+
+	}
+
+}
